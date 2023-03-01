e59c72d8f428bdea65758f20c1819b9ec11d468b
+ <lukas.kamau@student.moringaschool.com>
2023-03-01 20:50:07 +0300

    associated the user and the reviews class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-    has_many :bookings
-    has_many :apartments, through: :bookings
+  has_many :bookings
+  has_many :apartments, through: :bookings
+  has_many :reviews, class_name: "Review", foreign_key: "user_id"
+
+
   
   validates :firstname, :lastname, :username, :email, :password, presence: true
   validates :email, uniqueness: true
-  end
+end
   
