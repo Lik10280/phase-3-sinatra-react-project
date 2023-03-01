317e109da2fd19fb8ee878c52b33a41f6cc018a0
+ <lukas.kamau@student.moringaschool.com>
2023-03-01 20:32:12 +0300

    added validations to the users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
     has_many :bookings
     has_many :apartments, through: :bookings
   
-    
+  validates :firstname, :lastname, :username, :email, :password, presence: true
+  validates :email, uniqueness: true
   end
   
