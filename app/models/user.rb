09afcc674fa10e63987943ae14bd931cffacc45c
+ <lukas.kamau@student.moringaschool.com>
2023-03-06 11:10:46 +0300

    removed email validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
     has_many :bookings
     has_many :rentals, through: :bookings
     has_many :reviews, class_name: "Review", foreign_key: "user_id"
+    has_secure_password
   
   
     
     validates :firstname, :lastname, :username, :email, :password, presence: true
-    validates :email, uniqueness: true
+    
   end
